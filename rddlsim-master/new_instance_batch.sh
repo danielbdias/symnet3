ceb0efe772ed97723d28e167b59184c24d4a9a17
Daniel Dias <danielbpdias@gmail.com>
2025-07-31 00:51:13 -0300

    wip

--- rddlsim-master/new_instance_batch.sh
+++ rddlsim-master/new_instance_batch.sh
@@ -1,14 +1,17 @@
-!/bin/sh
+#!/bin/sh
+
+# fail if any errors
+set -e
 
 # declare -a domains=("academic_advising" "crossing_traffic" "game_of_life" "navigation" "skill_teaching" "sysadmin" "tamarisk" "traffic" "wildfire" "recon" "triangle_tireworld" "elevators")
 
-declare -a domains=("academic_advising")
+declare -a domains=("navigation")
 # declare -a instances=("701" "702" "703" "704" "705" "706" "707" "708" "709" "710" "711" "712")
 # declare -a instances=("821" "822" "823" "824" "825" "826" "827" "828" "828" "829" "830" "831" "832" "833" "834" "835" "836" "837" "838" "839" "840" "841" "842" "843" "844")
 # declare -a instances=("1000" "1001" "1002" "1003" "1004" "1005" "1006" "1007" "1008" "1009" "1010" "1011" "1013" "1014")
 # declare -a instances=("1001" "1002" "1003" "1005" "1007" "1008" "1009" "1010" "1011" "1012" "1014")
 # declare -a instances=("1001" "1002" "1005" "1006" "1007" "1013" "1014")
-declare -a instances=($(seq 900 950))
+declare -a instances=($(seq 700 950))
 
 for i in "${domains[@]}"
 do
